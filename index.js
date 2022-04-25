681230a0680162255276fc3a455c6738bb31003f
disapp4 <lyuba.mcr@gmail.com>
2022-04-25 19:42:10 +0300

    Добавлено ядро фронтенда

---
+++ index.js
@@ -0,0 +1,7 @@
+film_list = [
+  { id:1, title: , description:, },
+  { id:2, title: , description:,},
+  { id:3, title: , description:,},
+  { id:4, title: , description:,},
+  { id:5, title: , description:,}]
+  
