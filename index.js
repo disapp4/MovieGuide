8dc8b71ddbdb16088ade91819946ac68bad52de7
disapp4 <lyuba.mcr@gmail.com>
2022-05-11 19:32:48 +0300

    #3 #4 Изучение JQuery и добавление фильма

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-let film_list = [
-  { id: 1, title: 'Фильм 1', description: 'Описание 1', },
-  { id: 2, title: 'Фильм 2', description: 'Описание 2', },
-  { id: 3, title: 'Фильм 3', description: 'Описание 3', },
-  { id: 4, title: 'Фильм 4', description: 'Описание 4', },
-  { id: 5, title: 'Фильм 5', description: 'Описание 5', }];
+let film_list = [];
+
 
 function ViewFilms() {
+  $("#film_list").children().remove();
   for (let i = 0; i < film_list.length; i++) {
-    console.log('hjh')
-    document.getElementById("film_list")
-      .insertAdjacentHTML("beforeend",
-        `<div class="film">
-        <p> ` + film_list[i].title + `</p> 
-        </div > `);
-  }
 
+    let divClass = `<div class="film">
+    <p> ` + film_list[i].title +`</p> </div > `;
+    
+    $("#film_list").append(divClass);
+  }
+}
+
+function addMovieThroughForm() {
+  let idValue = $("#id_input").val();
+  let titleValue = $("#title_input").val();
+  let descriptionValue = $("#description_input").val();
+
+  let objMovie = { id: Number(idValue), title: titleValue, decription: descriptionValue };
+  film_list.push(objMovie);
+
+  $("#id_input,#title_input,#description_input").val(null);
+  ViewFilms();
 }
 ViewFilms();
