5bdfaa99f3e4d5b791eb4b875e8e6c3449b862da
disapp4 <lyuba.mcr@gmail.com>
2022-07-21 19:53:52 +0300

    #38 добавлен редирект и #17 постер

--- addMovie/addMovie.js
+++ addMovie/addMovie.js
@@ -1,18 +1,33 @@
+$("#image_to_save").on('change', putMoviePoster);
+let posterFile;
 function addMovieThroughForm() {
-    let titleValue = $("#title_input").val();
-    let descriptionValue = $("#description_input").val();
-  
-    if (titleValue == null || titleValue == "" || titleValue == undefined) {
-      alert("ошибка заполнения формы")
-    }
-    else {
-      callPostMovie({
-        title: titleValue,
-        description: descriptionValue
-      },
-        () => {
-          $("#title_input,#description_input").val(null);
-          window.location.href = "../index.html"
-        });
-    }
+  let titleValue = $("#title_input").val();
+  let descriptionValue = $("#description_input").val();
+
+  if (titleValue == null || titleValue == "" || titleValue == undefined) {
+    alert("ошибка заполнения формы")
   }
+  else {
+    callPostMovie({
+      title: titleValue,
+      description: descriptionValue
+    },
+      (response) => {
+        let movieId = response.id;
+        $("#title_input,#description_input").val(null);
+        if (posterFile != null) {
+          callPutMoviePoster(posterFile, movieId, () => window.location.href = "/index.html")
+        }
+        else {
+          window.location.href = "/index.html"
+        }
+
+      });
+  }
+}
+
+
+
+function putMoviePoster(event) {
+  posterFile = event.target.files[0];
+}
--- moviePage/moviePage.js
+++ moviePage/moviePage.js
@@ -1,13 +1,24 @@
 const params = new URLSearchParams(window.location.search);
 let movieId = params.get('movieId');
 
-function informationAboutMovie(id) {
-    callGetMovie(id, (movie) => {
-        $("#id_input1").val(id);
-        $("#title_input1").val(movie.title);
-        $("#description_input1").val(movie.description)
-      })
+
+  function getMovie() {
+  callGetMovie(movieId, (movie) => {
+    $("#id_input1").val(movieId);
+    $("#title_input1").val(movie.title);
+    $("#description_input1").val(movie.description)
+    setPoster(movie);
+  })
 }
 
+function setPoster(movie){
+  let posterId = movie.posterId;
+    if (posterId !== null) {
+      $("#poster").attr("src", "https://movie-guide-backend.ntrubkin.ru/movies/" + movieId + "/poster");
+    }
+    else {
+      $("#poster").attr("src", "../no_poster.jpg");
+    }
+}
 
-informationAboutMovie(movieId);
+getMovie();
