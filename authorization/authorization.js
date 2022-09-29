91b0dfdbe4e7fea3245f87ccf009e288889c05a1
disapp4 <lyuba.mcr@gmail.com>
2022-09-29 20:24:50 +0300

    #49 удалили ненужные файлы

--- authorization/authorization.js
+++
@@ -1,8 +0,0 @@
-
-
-function login() {
-    let username = $("#username").val();
-    let password = $("#password").val();
-    callLogIn(username, password, ()=> {window.location.href='/'} , ()=> alert("Неправильный логин или пароль"))
-
-}
