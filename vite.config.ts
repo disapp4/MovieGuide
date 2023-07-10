a3eb4a0666f3ab4c2531be1ea79c60e7282eeca7
disapp4 <lyuba.mcr@gmail.com>
2023-07-10 19:56:44 +0300

    #22 I18n- решена ошибка перезагрузки страницы при смене языка

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      external: ["loading.jpg"]}},
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
     },
   },
-  base: './'
 })
